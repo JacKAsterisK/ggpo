cd871be6d3a67b14405a37ee02e0cacf9ff89844
JacKAsterisK <jackasterisk@gmail.com>
2024-03-25 20:44:08 -0700

    Update configure_windows.cmd

--- configure_windows.cmd
+++ configure_windows.cmd
@@ -6,13 +6,21 @@ echo GGPO Visual Studio Solution Generator
 set GGPO_SHARED_LIB=off
 set STEAMWORKS_PATH=
 set NO_PROMPT=
+:check_file
+IF EXIST steamworks_path.txt (
+    FOR /F "tokens=*" %%G IN (steamworks_path.txt) DO SET STEAMWORKS_PATH=%%G
+) ELSE (
+    echo File steamworks_path.txt not found.
+    goto end
+)
+
 
 :parse_args
 IF "%~1"=="" GOTO :start
 IF "%1"=="--no-prompt" SET NO_PROMPT=1 & SHIFT & GOTO :parse_args
-SET STEAMWORKS_PATH=%1
-REM Replace | with space
-SET STEAMWORKS_PATH=%STEAMWORKS_PATH:|= %
+SET STEAMWORKS_PATH=%
+REM Replace ~ with space
+SET STEAMWORKS_PATH=%STEAMWORKS_PATH:~= %
 SHIFT
 GOTO :parse_args
 
@@ -26,6 +34,8 @@ echo    GGPO_SHARED_LIB=%GGPO_SHARED_LIB%
 echo    Steamworks Path: %STEAMWORKS_PATH%
 
 cmake -G "Visual Studio 17 2022" -A x64 -B build -DBUILD_SHARED_LIBS=%GGPO_SHARED_LIB% -DSTEAMWORKS_PATH="%STEAMWORKS_PATH%"
+"
+PATH="%STEAMWORKS_PATH%"
 
 echo Finished!  Open build/GGPO.sln in Visual Studio to build.
 
